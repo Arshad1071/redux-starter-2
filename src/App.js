f8a2825db78cdf7285d80a568d2585d89c787857
Arshad vp <Arshad1071@yahoo.in>
2024-02-20 15:35:53 +0400

    new routes added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Profile from "./pages/profile";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
